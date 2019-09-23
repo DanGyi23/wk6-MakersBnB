2ff8238411d404e310a5d25dba66225e14e34c1e
Dan Gyi <daniel.gyi@gmail.com>
2019-09-23 15:37:51 +0100

    updates script order on index/interface

--- interface.js
+++ interface.js
@@ -17,13 +17,16 @@ $(document).ready(function () {
 
     //update next line when feature implemented
     $('loggedinuser').text("Dan, Volker, Ben")
+    indexFill();
 
+    indexFill = function() {
     for (i = 1; i < 5; i++) {
       $('#listing' + i + '-title').text(data.property_name)
       // $('#listing' + i + '-thumb').so(data.image_path)
       $('#property' + i + 'location').text(data.location)
       $('#property' + i + 'price').text(data.price_per_night)
     }
+  }
 
-  });
+  })
 });
