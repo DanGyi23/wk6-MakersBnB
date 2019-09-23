8e00e7d26f371edaed2c37641b973f31afa9755f
Ben <ben@benzeng.co.uk>
2019-09-23 14:33:58 +0100

    fixed spec_helper, now works with simple-cov and capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,20 @@
 # require './spec/setup_test_database.rb'
 ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
-
 require File.join(File.dirname(__FILE__), '..' , 'server.rb')
 
+require 'simplecov'
+require 'simplecov-console'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-require 'simplecov'
-require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::Console,
+# Want a nice code coverage website? Uncomment this next line!
+# SimpleCov::Formatter::HTMLFormatter
+])
 
 SimpleCov.start
-
 Capybara.app = Server
 
 RSpec.configure do |config|
@@ -20,17 +23,8 @@ RSpec.configure do |config|
   #   setup_test_database
   # end
 
-  config.expect_with :rspec do |expectations|
-
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.after(:suite) do
+    puts ""
+    puts "rspec tests complete"
   end
-
-  config.mock_with :rspec do |mocks|
-
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-
 end
