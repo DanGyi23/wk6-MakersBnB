f9b6dae3f91cb988e6777d4506002b31c59e676a
Dan Gyi <daniel.gyi@gmail.com>
2019-09-23 15:28:56 +0100

    add log to interface

--- interface.js
+++ interface.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
   $.get('http://localhost:9292', function (data) {
+    console.log(data)
     $('#dbpropertyname').text(data.property_name)
     $('#dbpropertydescription').text(data.property_description)
     $('#dbprice').text(data.price_per_night)
@@ -19,7 +20,7 @@ $(document).ready(function () {
 
     for (i = 1; i < 5; i++) {
       $('#listing' + i + '-title').text(data.property_name)
-      // $('#listing' + i + '-thumb').text(data.property_name)
+      // $('#listing' + i + '-thumb').so(data.image_path)
       $('#property' + i + 'location').text(data.location)
       $('#property' + i + 'price').text(data.price_per_night)
     }
