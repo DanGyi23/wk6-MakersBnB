49a3d36baa7f4e4ebf339ed767b40899fed455e5
Volker Bergner <volker@bergner.one>
2019-09-23 15:24:46 +0100

    Added test data with Daniels place

--- db/migrations/02_insert_test_values.sql
+++ db/migrations/02_insert_test_values.sql
@@ -1,3 +1,4 @@
 INSERT INTO properties VALUES (default, 'Bens place', 'Bens fantastic place etc etc etc etc', 99, 'entire flat', 4, 'London', true, 100, 2, 2, true, true);
 INSERT INTO properties VALUES (default, 'Volkers place', 'Volkers great place etc etc etc etc', 97, 'shared flat', 4, 'Berlin', true, 90, 2, 2, true, true);
 INSERT INTO properties VALUES (default, 'Dans place', 'Dans awesome place etc etc etc etc', 97, 'entire flat', 4, 'London', true, 101, 2, 3, false, true);
+INSERT INTO properties VALUES (default, 'Daniels place', 'Daniels wonderful place etc etc etc etc', 200, 'entire flat', 4, 'London', true, 101, 2, 3, true, true);
