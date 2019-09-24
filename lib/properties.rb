05e608c9daf61f7ea37bb31df95d00caec91de5c
Ben <ben@benzeng.co.uk>
2019-09-24 10:04:02 +0100

    refactored properties to properties

--- lib/properties.rb
+++ lib/properties.rb
@@ -4,11 +4,18 @@ require './lib/database_connection_setup.rb'
 
 class Properties
 
-  def self.all
+  def self.all_properties
     @all_properties = []
     result = DatabaseConnection.query('select * from properties')
     result.map { |properties| @all_properties << properties }
     return @all_properties
   end
+
+  # def self.all
+  #   @all_properties = []
+  #   result = DatabaseConnection.query('select * from properties')
+  #   result.map { |properties| @all_properties << properties }
+  #   return @all_properties
+  # end
 
 end
--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -4,9 +4,9 @@ require './lib/properties.rb'
 describe Properties do
   describe '#all' do
     it 'returns test db entries' do
-      properties = Properties.all
-      expect(properties[0]["property_name"]).to eq("Bens place")
-      expect(properties[1]["property_name"]).to eq("Volkers place")
+      all_properties = Properties.all_properties
+      expect(all_properties[0]["property_name"]).to eq("Bens place")
+      expect(all_properties[1]["property_name"]).to eq("Volkers place")
     end
   end
 end
