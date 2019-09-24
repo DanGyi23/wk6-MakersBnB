d74974a555da7985100e075a4aa507f9cbeae710
Ben <ben@benzeng.co.uk>
2019-09-24 16:16:23 +0100

    added dynamic generation of listings

--- index-interface.js
+++ index-interface.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
 
   $.get('http://localhost:9292/properties', function (data) {
 
+    let $listingcontainer = $("<div/>", { class: "listingcontainer" }),
+        $listingthumbnail = $("<img/>", { class: "listingthumbnail", id: "listing1thumb", src: "https://i.ibb.co/FJRwL46/makers-logo-lockupv-black.png", border: "0"}),
+        $link = $("<a/>", { class: "link", id: "property1id", href: "./listing.html"}),
+        $linktitle = $("<h3/>", { id: "listing1-title", text: "Property Name D"}),
+        $propertylocation = $("<p/>", { id: "property1location", text: "Property Location D" }),
+        $price = $("<p/>", { id: "property1price", text: "Placeholder Price D"});
+
+    for (let i = 0; i < data.length; i++) {
+      $listingcontainer.append($listingthumbnail, $link.append($linktitle), $propertylocation, $price).appendTo( '#listings' );
+      $listingcontainer.append($listingthumbnail, $link.append($linktitle), $propertylocation, $price).appendTo( '#listings' );
+    }
+
 
     for (let i = 0; i < data.length; i++) {
       $('#listing' + (i + 1) + '-title').text(data[i].property_name);
@@ -16,5 +28,9 @@ $(document).ready(function () {
       console.log(id);
       window.localStorage.setItem('id', id)
     });
+
+
+
+
   });
 });
