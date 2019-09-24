37f6012dda0d49d14baad8ca67d79a74e5d7dd20
Dan Gyi <daniel.gyi@gmail.com>
2019-09-24 12:31:03 +0100

    storing id dynamically on index listing click

--- index-interface.js
+++ index-interface.js
@@ -3,19 +3,17 @@ $(document).ready(function () {
   $.get('http://localhost:9292/properties', function (data) {
 
 
-    for (i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       $('#listing' + (i + 1) + '-title').text(data[i].property_name)
       // $('#listing' + i + '-thumb').so(data.image_path)
       $('#property' + (i + 1) + 'location').text(data[i].location)
       $('#property' + (i + 1) + 'price').text("$" + data[i].price_per_night + " per night")
-      // $("#property" + i + "id").attr(name , data.id)
-    };
-
-    for (i = 1; i < data.length; i++) {
-      $('#listing' + i + '-title').click(function () {
-        $.get('http://localhost/properties/' + i)
-        window.localStorage.setItem('id', i)
-      })
-    };
+      $("#property" + (i + 1) + "id").attr('id' , data[i].id)
+  };
+    $('.link').click(function () {
+      var id = $(this).attr('id');
+      console.log(id)
+      window.localStorage.setItem('id', id)
+    });
   });
-})
+});
