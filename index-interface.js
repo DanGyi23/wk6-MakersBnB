8cccc33a9a3e3d02c4972dfb79aea8404aa748db
Ben <ben@benzeng.co.uk>
2019-09-24 14:39:18 +0100

    syntax correction

--- index-interface.js
+++ index-interface.js
@@ -4,15 +4,16 @@ $(document).ready(function () {
 
 
     for (let i = 0; i < data.length; i++) {
-      $('#listing' + (i + 1) + '-title').text(data[i].property_name)
+      $('#listing' + (i + 1) + '-title').text(data[i].property_name);
       // $('#listing' + i + '-thumb').so(data.image_path)
-      $('#property' + (i + 1) + 'location').text(data[i].location)
-      $('#property' + (i + 1) + 'price').text("$" + data[i].price_per_night + " per night")
+      $('#property' + (i + 1) + 'location').text(data[i].location);
+      $('#property' + (i + 1) + 'price').text("$" + data[i].price_per_night + " per night");
       $("#property" + (i + 1) + "id").attr('id' , data[i].id)
-  };
+  }
+
     $('.link').click(function () {
       var id = $(this).attr('id');
-      console.log(id)
+      console.log(id);
       window.localStorage.setItem('id', id)
     });
   });
