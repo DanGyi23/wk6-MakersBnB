f08de7a4f1392f788853a580077b77f4f7946f2e
Dan Gyi <daniel.gyi@gmail.com>
2019-09-25 16:05:05 +0100

    create listing interface now posts

--- create-listing-interface.js
+++ create-listing-interface.js
@@ -3,6 +3,18 @@ $(document).ready(function () {
   $('#createnewlistingform').submit(function (event) {
     event.preventDefault();
 
+    if ($('#wifi').val() == "Yes") {
+      wifi = "TRUE"
+    } else {
+      wifi = "FALSE"
+    }
+
+    if ($('#washing_machine').val() == "Yes") {
+      washing_machine = "TRUE"
+    } else {
+      washing_machine = "FALSE"
+    }
+
     $.post('http://localhost:9292/newlistingsubmit', {
       property_name: $('#property_name').val(),
       property_type: $('#property_type').val(),
@@ -13,10 +25,10 @@ $(document).ready(function () {
       size: $('#size').val(),
       bathrooms: $('#bathrooms').val(),
       beds: $('#beds').val(),
-      wifi: $('#wifi'),
-      washing_machine: $('washing_machine').val()
+      wifi: wifi,
+      washing_machine: washing_machine
     }
-    
+
     );
   });
 });
