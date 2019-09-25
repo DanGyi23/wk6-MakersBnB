745b729f528b38f0952caf4af59c748e7002af6d
Dan Gyi <daniel.gyi@gmail.com>
2019-09-25 15:57:37 +0100

    post new listing to server

--- create-listing-interface.js
+++ create-listing-interface.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
-  $('#createnewlistingform').submit(function() {
-    property_name = $('#property_name')
-    property_description = $('#property_description').val()
-    price_per_night = $('#price_per_night').val()
-    property_type = $('#property_type').val()
-    capacity = $('#capacity').val()
-    location = $('#location').val()
-    size = $('#size').val()
-    bathrooms = $('#bathrooms').val()
-    beds = $('#beds').val()
-    wifi = $('#wifi')
-    washing_machine = $('washing_machine').val()
-  })
+  $('#createnewlistingform').submit(function (event) {
+    event.preventDefault();
+
+    $.post('http://localhost:9292/newlistingsubmit', {
+      property_name: $('#property_name').val(),
+      property_type: $('#property_type').val(),
+      property_description: $('#property_description').val(),
+      price: $('#price_per_night').val(),
+      capacity: $('#capacity').val(),
+      location: $('#location').val(),
+      size: $('#size').val(),
+      bathrooms: $('#bathrooms').val(),
+      beds: $('#beds').val(),
+      wifi: $('#wifi'),
+      washing_machine: $('washing_machine').val()
+    }
+    
+    );
+  });
 });
