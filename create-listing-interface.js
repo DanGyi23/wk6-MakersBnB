d89129446a858d0017a6ec6b6e80bcfa974146c1
Dan Gyi <daniel.gyi@gmail.com>
2019-09-25 12:40:18 +0100

    add listing creation and confirmation pages

---
+++ create-listing-interface.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $('#createnewlistingform').submit(function() {
+    $('#price_per_night').val()
+    $('#property_type').val()
+    $('#capacity').val()
+    $('#location').val()
+    $('#size').val()
+    $('#bathrooms').val()
+    $('#beds').val()
+    $('washing_machine').val()
+  })
+});
