8764212497531e1a57d2f6181685f69ca9999431
Ben <ben@benzeng.co.uk>
2019-09-25 17:02:28 +0100

    js now catches 401 on failed login

--- index-interface.js
+++ index-interface.js
@@ -3,14 +3,19 @@ $(document).ready(function () {
   $.get('http://localhost:9292/properties', function (data) {
 
     for (let i = 1; i <= data.length; i++) {
-      let $listingcontainer = $("<div/>", { class: "listingcontainer" }),
-          $listingthumbnail = $("<img/>", { class: "listingthumbnail", id: `listing${i}thumb`, src: "https://i.ibb.co/FJRwL46/makers-logo-lockupv-black.png", border: "0"}),
-          $link = $("<a/>", { class: "link", id: `property${i}id`, href: "./listing.html"}),
-          $linktitle = $("<h3/>", { id: `listing${i}-title`, text: "Property Name D"}),
-          $propertylocation = $("<p/>", { id: `property${i}location`, text: "Property Location D" }),
-          $price = $("<p/>", { id: `property${i}price`, text: "Placeholder Price D"});
+      let $listingcontainer = $("<div/>", {class: "listingcontainer"}),
+          $listingthumbnail = $("<img/>", {
+            class: "listingthumbnail",
+            id: `listing${i}thumb`,
+            src: "https://i.ibb.co/FJRwL46/makers-logo-lockupv-black.png",
+            border: "0"
+          }),
+          $link = $("<a/>", {class: "link", id: `property${i}id`, href: "./listing.html"}),
+          $linktitle = $("<h3/>", {id: `listing${i}-title`, text: "Property Name D"}),
+          $propertylocation = $("<p/>", {id: `property${i}location`, text: "Property Location D"}),
+          $price = $("<p/>", {id: `property${i}price`, text: "Placeholder Price D"});
 
-      $listingcontainer.append($listingthumbnail, $link.append($linktitle), $propertylocation, $price).appendTo( '#listings' );
+      $listingcontainer.append($listingthumbnail, $link.append($linktitle), $propertylocation, $price).appendTo('#listings');
     }
 
     for (let i = 0; i < data.length; i++) {
@@ -18,33 +23,43 @@ $(document).ready(function () {
       // $('#listing' + i + '-thumb').so(data.image_path)
       $('#property' + (i + 1) + 'location').text(data[i].location);
       $('#property' + (i + 1) + 'price').text("$" + data[i].price_per_night + " per night");
-      $("#property" + (i + 1) + "id").attr('id' , data[i].id)
-  }
+      $("#property" + (i + 1) + "id").attr('id', data[i].id)
+    }
 
-    $('#loginform').submit(function(event) {
+    $('#loginform').submit(function (event) {
       event.preventDefault();
       let email = $('#email').val(),
           password = $('#password').val();
-      $.post('http://localhost:9292/login', { email: email, password: password },
-            function(response){alert("Login successful")});
-    });
+      // $.post('http://localhost:9292/login', { email: email, password: password },
+      //       function(response){alert("Login successful")});
+      $.post("http://localhost:9292/login", {email: email, password: password})
+          .done(function () {
+            alert("Login Successful");
+          })
+          .fail(function () {
+            alert("401 authentication error");
+          });
 
-    $('#signupform').submit(function(event) {
-      event.preventDefault();
-      let name = $('#name').val(),
-          email = $('#email').val(),
-          password = $('#password').val();
-      if ($('#password').val() === $('#passwordconfirm').val()) {
-        $.post('http://localhost:9292/signup', { name: name, email: email, password: password },
-            function(response){alert("Sign up successful you are now logged in")});
-      } else {
-        alert("Passwords Do Not Match!")
-      };
-    });
+      $('#signupform').submit(function (event) {
+        event.preventDefault();
+        let name = $('#name').val(),
+            email = $('#email').val(),
+            password = $('#password').val();
+        if ($('#password').val() === $('#passwordconfirm').val()) {
+          $.post('http://localhost:9292/signup', {name: name, email: email, password: password},
+              function (response) {
+                alert("Sign up successful you are now logged in")
+              });
+        } else {
+          alert("Passwords Do Not Match!")
+        }
+        ;
+      });
 
-    $('.link').click(function () {
-      var id = $(this).attr('id');
-      window.localStorage.setItem('id', id)
+      $('.link').click(function () {
+        var id = $(this).attr('id');
+        window.localStorage.setItem('id', id)
+      });
     });
   });
 });
